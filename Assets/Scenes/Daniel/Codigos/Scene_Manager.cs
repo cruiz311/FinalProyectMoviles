01a7f515202982af2b11012b2e1765cb5219d5c5
Danielc26-dev <144168063+Danielc26-dev@users.noreply.github.com>
2023-12-10 22:41:51 -0500

    Daniel_Brunch
    Anadido movimiento en el plano del jugador, cambio de escena con un singelton "Creo que se llama asi" y los assets a, hare un canvas con los assets para que sea mas ordenado

---
+++ Assets/Scenes/Daniel/Codigos/Scene_Manager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public static Scene_Manager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsyncCoroutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsyncCoroutine(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100f) + "%");
+
+            yield return null;
+        }
+    }
+}
