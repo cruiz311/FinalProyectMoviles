e2f277bb5dbb403aa63b3ca2a2b90c3e5ab5aa31
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2023-12-15 03:42:16 -0500

    EnemysScriptableObjects

--- Assets/Miguel/ScriptsM/EnemyStructure/Enemys.cs
+++ Assets/Miguel/ScriptsM/EnemyStructure/Enemys.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 
 public class Enemys : MonoBehaviour
 {
-    protected int vida;
-    protected int maxVida;
-    protected int damage;
-    protected int velocidad;
-    public EnemyData EnemyData; // Referencia al Scriptable Object de estadísticas de enemigo
+    public int vida;
+    public int maxVida;
+    public int damage;
+    public int velocidad;
     protected Transform jugadorPos;
 
-
-    private void Awake()
-    {
-        EnemyData.Awake();
-    }
     private void Start()
     {
         jugadorPos = GameObject.FindGameObjectWithTag("Player").transform;
-
-        if (EnemyData != null)
-        {
-            vida = EnemyData.vida;
-            maxVida = EnemyData.maxVida;
-            damage = EnemyData.damage;
-            velocidad = EnemyData.velocidad;
-            // Asigna otras estadísticas según sea necesario para el enemigo
-        }
     }
 }
