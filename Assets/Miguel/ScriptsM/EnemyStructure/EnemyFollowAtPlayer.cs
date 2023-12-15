8fe4b4a84100232dae1182f41eda05a728636586
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2023-12-15 02:00:04 -0500

    Corregir Animaciones Daniel

--- Assets/Miguel/ScriptsM/EnemyStructure/EnemyFollowAtPlayer.cs
+++ Assets/Miguel/ScriptsM/EnemyStructure/EnemyFollowAtPlayer.cs
@@ -1,21 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyFollowAtPlayer : Enemys
 {
-    // Update is called once per frame
+    public float distanciaDeAtaque = 5.0f; // La distancia a la que el enemigo atacará
+
     void Update()
     {
         if (jugadorPos != null)
         {
-            transform.LookAt(jugadorPos);
-            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+            // Calcula la distancia entre el enemigo y el jugador
+            float distancia = Vector3.Distance(transform.position, jugadorPos.position);
+
+            if (distancia <= distanciaDeAtaque)
+            {
+                Debug.Log("¡Ataque!");
+                StartCoroutine(AttackMele());
+            }
+            else
+            {
+                Vector3 direction = jugadorPos.transform.position - transform.position;
+                direction.y = 0;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * 5f);
+
+                transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+            }
+
+            
         }
     }
 
     IEnumerator AttackMele()
     {
+        // Aquí puedes poner la lógica de tu ataque, por ejemplo:
+        // Instanciar un proyectil, hacer daño al jugador, etc.
         yield return new WaitForSeconds(1.0f);
     }
 }
